4a8e1f718e4e06cd410b49b0deb3e3f3d1e755b2
Megafont <51346737+Megafont@users.noreply.github.com>
2024-09-03 16:20:17 -0500

    Finished chapter 6 in the course

--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -101,7 +101,8 @@
 //   return insertedRevenue;
 // }
 
-export async function GET() {
+export async function GET()
+{
   return Response.json({
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
