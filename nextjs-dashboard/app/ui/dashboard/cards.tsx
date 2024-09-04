6bf6238d92c0323390c62a5c2b3d81e46b383a0e
Megafont <51346737+Megafont@users.noreply.github.com>
2024-09-04 17:53:44 -0500

    Finished chapter 10 of the course

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -1,10 +1,12 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+import
+  {
+    BanknotesIcon,
+    ClockIcon,
+    UserGroupIcon,
+    InboxIcon,
+  } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,19 +15,26 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+export default async function CardWrapper()
+{
+  const
+    {
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -38,7 +47,8 @@ export function Card({
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+})
+{
   const Icon = iconMap[type];
 
   return (
